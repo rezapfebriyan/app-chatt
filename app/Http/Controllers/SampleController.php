bd522f12cf7fa01a3d25b0a42277b80f48a3a7f5
rezapfebriyan <rezafebriyan00@gmail.com>
2022-11-16 03:33:09 +0700

    add routes and method register, login, logout

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -31,7 +31,8 @@ class SampleController extends Controller
         User::create([
             'name'     => $data['name'],
             'email'    => $data['email'],
-            'password' => Hash::make($data['password'])
+            'password' => Hash::make($data['password']),
+            'token'    => md5(uniqid())
         ]);
 
         return redirect('login')->with('success', 'Registration Completed, now you can login');
@@ -73,48 +74,5 @@ class SampleController extends Controller
         auth()->logout();
 
         return Redirect('login');
-    }
-
-    public function profile()
-    {
-        if(auth()->check())
-        {
-            $data = User::where('id', auth()->id())->get();
-
-            return view('profile', compact('data'));
-        }
-
-        return redirect("login")->with('success', 'you are not allowed to access');
-    }
-
-    public function profile_validation(Request $request)
-    {
-        $request->validate([
-            'name'       => 'required',
-            'email'      => 'required|email',
-            'user_image' => 'image|mimes:jpg,png,jpeg|max:2048|dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000'
-        ]);
-
-        $user_image = $request->hidden_user_image;
-
-        if($request->user_image != '')
-        {
-            $user_image = time() . '.' . $request->user_image->getClientOriginalExtension();
-            $request->user_image->move(public_path('images'), $user_image);
-        }
-
-        $user = User::find(auth()->id());
-        $user->name = $request->name;
-        $user->email = $request->email;
-
-        if($request->password != '')
-        {
-            $user->password = Hash::make($request->password);
-        }
-
-        $user->user_image = $user_image;
-        $user->save();
-
-        return redirect('profile')->with('success', 'Profile Details Updated');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(App\Http\Controllers\SampleController::class)->group(function(){
+    Route::get('registration', 'registration')->name('registration');
+    Route::get('login', 'index')->name('login');
+    Route::get('logout', 'logout')->name('logout');
+
+    Route::post('validate_registration', 'validate_registration')->name('sample.validate_registration');
+    Route::post('validate_login', 'validate_login')->name('sample.validate_login');
+
+    Route::get('dashboard', 'dashboard')->name('dashboard');
+
+    Route::get('profile', 'profile')->name('profile');
+    Route::post('profile_validation', 'profile_validation')->name('sample.profile_validation');
 });
