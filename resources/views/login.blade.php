65c24997148bcd5a7ae724f028d23212ac425444
rezapfebriyan <rezafebriyan00@gmail.com>
2022-11-16 03:52:34 +0700

    update UI page dashboard, login and profile

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,41 +1,38 @@
 @extends('main')
 
 @section('content')
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ $message }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
 
-@if($message = Session::get('success'))
-
-<div class="alert alert-info">
-{{ $message }}
-</div>
-
-@endif
-
-<div class="row justify-content-center">
-	<div class="col-md-4">
-		<div class="card">
-			<div class="card-header">Login</div>
-			<div class="card-body">
-				<form action="{{ route('sample.validate_login') }}" method="post">
-					@csrf
-					<div class="form-group mb-3">
-						<input type="text" name="email" class="form-control" placeholder="Email" />
-						@if($errors->has('email'))
-							<span class="text-danger">{{ $errors->first('email') }}</span>
-						@endif
-					</div>
-					<div class="form-group mb-3">
-						<input type="password" name="password" class="form-control" placeholder="Password" />
-						@if($errors->has('password'))
-							<span class="text-danger">{{ $errors->first('password') }}</span>
-						@endif
-					</div>
-					<div class="d-grid mx-auto">
-						<button type="subit" class="btn btn-dark btn-block">Login</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-</div>
-
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Login</div>
+                <div class="card-body">
+                    <form action="{{ route('sample.validate_login') }}" method="post">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <input type="text" name="email" class="form-control" placeholder="Email" />
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="password" name="password" class="form-control" placeholder="Password" />
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                        <div class="d-grid mx-auto">
+                            <button type="subit" class="btn btn-dark btn-block">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection('content')
