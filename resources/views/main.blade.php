05bbd87da851941c3b9ff8b9580a8d832bb45e3e
rezapfebriyan <rezafebriyan00@gmail.com>
2022-11-29 14:42:25 +0700

    add hyperlink for page dashboard

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -18,7 +18,7 @@
 
     <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
         <div class="container">
-            <a class="navbar-brand mr-auto" href="#">Chat Application</a>
+            <a class="navbar-brand mr-auto" href="{{ route('dashboard') }}">Chat Application</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
